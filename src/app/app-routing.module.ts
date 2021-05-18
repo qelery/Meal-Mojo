ee502e5e261e73e0fa6644f76ff8740baeeff97a
Quincy Elery <quincyelery@gmail.com>
2021-05-17 20:01:19 -0500

    basic routing for signin card

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {SigninCardComponent} from "./signin-card/signin-card.component";
+import {HomeComponent} from "./home/home.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'register',
+    component: SigninCardComponent
+  },
+  {
+    path: 'login',
+    component: SigninCardComponent
+  },
+  {
+    path: '',
+    component: HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
+
+
+
 export class AppRoutingModule { }
